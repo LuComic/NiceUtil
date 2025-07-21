e778878e6e888943ec2fb27901b39c855dd17f5a
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-07-21 14:26:13 +0300

    Initial Commit

---
+++ NiceUtil/NiceUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceUtilApp.swift
+//  NiceUtil
+//
+//  Created by Lukas Jääger on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NiceUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NiceUtilTests/NiceUtilTests.swift
@@ -0,0 +1,17 @@
+//
+//  NiceUtilTests.swift
+//  NiceUtilTests
+//
+//  Created by Lukas Jääger on 21.07.2025.
+//
+
+import Testing
+@testable import NiceUtil
+
+struct NiceUtilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
